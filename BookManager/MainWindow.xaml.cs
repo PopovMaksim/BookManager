b2f57d2e74e8d9c060d306e3d4edd13118551df7
PopovMaksim <maksimpopov052006@gmail.com >
2025-05-03 14:50:44 +0300

    Наброски інтерфейсу

--- BookManager/MainWindow.xaml.cs
+++ BookManager/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,18 +11,35 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BookManager;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace BookManager
 {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+			ObservableCollection<Book> books = new ObservableCollection<Book>();
+
+			books.Add(new Book { code = "2", author="dsd", title="lkfj", year="jkf", rack="skjdf", shelf="klsj"});
+			BooksGataGrid.ItemsSource = books;
+		}
     }
+
+	public class Book {
+		public required string code { get; set; }
+		public required string author { get; set; }
+		public required string title { get; set; }
+		public required string year { get; set; }
+		public required string rack { get; set; }
+		public required string shelf { get; set; }
+	}
+
+
+
 }
