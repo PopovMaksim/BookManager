c711b4d15f0be773f4a2511f27b9c10a24f537ff
Nikita <tsebro.nikita@student.sumdu.edu.ua>
2025-05-04 23:39:13 +0300

    Додав форму видалення

---
+++ BookManager/DeleteWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookManager
+{
+    /// <summary>
+    /// Interaction logic for DeleteWindow.xaml
+    /// </summary>
+    public partial class DeleteWindow : Window
+    {
+        bool isConfirmed;
+        public DeleteWindow()
+        {
+            InitializeComponent();
+        }
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Запис видалено");
+            this.Close();
+        }
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
